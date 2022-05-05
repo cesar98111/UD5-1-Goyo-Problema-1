7d9e2facdd65c67d0ea044aa8dbbe20da16494c0
cesar jesus leon martin <cesilius98@gmail.com>
2022-05-05 11:05:06 +0200

    Feature: implementacion de la cola en el main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import com.company.dataEstrucuture.Queue;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        Queue cola = new Queue();
+        int exit = 1;
+        String numero="";
+        while(exit!=0){
+            cola.enQueue(Float.valueOf(sc.nextLine()));
+            if(numero.equals("q")){
+                exit=0;
+            }
+        }
     }
 }
--- src/com/company/dataEstrucuture/Queue.java
+++ src/com/company/dataEstrucuture/Queue.java
@@ -1,6 +1,6 @@
 package com.company.dataEstrucuture;
 
-public class GenericQueue {
+public class Queue {
     private static final int CAPACIDAD_INICIAL =5;
     private static final int TOP = 0;
     private CustomCollection collection = new CustomCollection(CAPACIDAD_INICIAL);
