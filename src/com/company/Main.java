05a0ea150548d642825e119b056da2835c075935
cesar jesus leon martin <cesilius98@gmail.com>
2022-05-05 11:16:07 +0200

    Fix: arreglado problema con la imlementacion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Queue cola = new Queue();
         int exit = 1;
+        int count=0;
         String numero="";
         while(exit!=0){
-            cola.enQueue(Float.valueOf(sc.nextLine()));
+            numero =sc.nextLine();
             if(numero.equals("q")){
                 exit=0;
+            }else{
+                cola.enQueue(Float.valueOf(numero));
+                count ++;
             }
+
+
+        }
+        for(int i=0;i<count;i++){
+            float hola = cola.deQueue();
+            System.out.println(hola);
         }
     }
 }
